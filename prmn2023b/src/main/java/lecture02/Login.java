1e8fcbe9db80ca3df9d9f0be9e727b4ab743284f
shun4193 <shun.h.1126.4193@rakuten.jp>
2023-11-02 12:53:21 +0900

    lecture02修正

--- prmn2023b/src/main/java/lecture02/Login.java
+++ prmn2023b/src/main/java/lecture02/Login.java
@@ -1,45 +1,36 @@
 package lecture02;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Login {
 
-    String number, password;
-    private String[] studentNumbers, passwords;
-    private HashMap<String, String> numberAndPasswords = new HashMap<String, String>();
+    private String[][] studentNumberAnsPasswords = new String[][]{
+            {"B2221950", "aiueo"},
+            {"B2225043", "kakikukeko"},
+            {"B2221435", "sasisuseso"},
+            {"B2225341", "tatituteto"},
+            {"B2226435", "naninuneno"},
+    };
+
     private Scanner scan = new Scanner(System.in);
 
-    Login() {
-        studentNumbers = new String[]{
-                "B2221950",
-                "B2225043",
-                "B2221435",
-                "B2225341",
-                "B2226435",
-        };
-
-        passwords =  new String[]{
-                "aiueo",
-                "kakikukeko",
-                "sasisuseso",
-                "tatituteto",
-                "naninuneno",
-        };
-
-        for (int i = 0; i < passwords.length; i++) numberAndPasswords.put(studentNumbers[i], passwords[i]);
-    }
-
-
-    boolean exisistNumber() {
+    int exisistNumber() {
         System.out.print("学籍番号を入力してください : ");
-        number = scan.next();
-        return numberAndPasswords.containsKey(number);
+        String number = scan.next();
+
+        int matchIndex = -1;
+        for(int i = 0; i < studentNumberAnsPasswords.length; i++) {
+            if(number.equals(studentNumberAnsPasswords[i][0])) {
+                matchIndex = i;
+                break;
+            }
+        }
+        return matchIndex;
     }
 
-    boolean exisistPassword() {
+    boolean exisistPassword(int index) {
         System.out.print("パスワードを入力してください : ");
-        password = scan.next();
-        return numberAndPasswords.get(number).equals(password);
+        String password = scan.next();
+        return studentNumberAnsPasswords[index][1].equals(password);
     }
 }
--- prmn2023b/src/main/java/lecture02/prac3.java
+++ prmn2023b/src/main/java/lecture02/prac3.java
@@ -4,12 +4,13 @@ public class prac3 {
     public static void main(String[] args) {
         Login login = new Login();
 
-        if (!login.exisistNumber()) {
+        int matchIndex = login.exisistNumber();
+        if (matchIndex == -1) {
             System.out.println("error!!");
             System.exit(0);
         }
 
-        if (!login.exisistPassword()) {
+        if (!login.exisistPassword(matchIndex)) {
             System.out.println("不正なアクセス");
             System.exit(0);
         }
